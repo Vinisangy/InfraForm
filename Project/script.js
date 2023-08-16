b0c884cca30033f8d7e0876659ece1711625f75b
vinisangy <vinisangy@gmail.com>
2023-08-16 09:56:54 -0300

    Atualizando

--- Project/script.js
+++ Project/script.js
@@ -1,1 +1,15 @@
 
+
+
+
+
+function showTipoInfra() {
+    var cloud = document.getElementById("cloud");
+    var camposOpcao1 = document.getElementById("camposOpcao1");
+
+    if (cloud.checked) {
+        camposOpcao1.style.display = "block";
+    } else {
+        camposOpcao1.style.display = "none";
+    }
+}
